f1c9fdba8defa95c60a0f6510fa7aadfcbfa1ed5
twiceborn.phoenix <twiceborn.phoenix@gmai.com>
2018-05-22 03:25:51 +0300

    class Actor, fix method plus

--- game.js
+++ game.js
@@ -9,9 +9,9 @@ class Vector {
   plus(obj) {
     try {
       if (obj instanceof Vector) {
-        obj.x += this.x;
-        obj.y += this.y;
-        return new Vector(obj.x, obj.y);
+        let newX = this.x + obj.x;
+        let newY = this.y + obj.y;
+        return new Vector(newX, newY);
       } else {
         console.log(e); 
       }
@@ -23,5 +23,51 @@ class Vector {
     this.x *= number;
     this.y *= number;
     return new Vector(this.x, this.y);
+  }
+}
+
+class Actor {
+  constructor(...args) {
+    if (typeof arguments[0] === "undefined") { 
+      this.pos = new Vector(0, 0);
+    } else {
+      this.pos = arguments[0];
+    }
+    if (typeof arguments[1] === "undefined") {
+      this.size = new Vector(1, 1);
+    } else {
+      this.size = arguments[1];
+    }
+    if (typeof arguments[2] === "undefined") {
+      this.speed = new Vector(0, 0);
+    } else {
+      this.speed = arguments[2];
+    }
+  }
+  act() {   
+  }
+  get left() {
+    return this.pos.x;
+  }
+  get right() {
+    return this.pos.x + this.size.x;
+  }
+  get top() {
+    return this.pos.y;
+  }
+  get bottom() {
+    return this.pos.y + this.size.y;
+  }
+  get type() {
+    return "actor";
+  }
+  isIntersect(obj) {
+    if (this === obj) {
+      return false;
+    }
+     if (this.right > obj.left && this.left < obj.right && this.top < obj.bottom && this.bottom > obj.top) {
+        return true;
+      }
+      return false;
   }
 }
